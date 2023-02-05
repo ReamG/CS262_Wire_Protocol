21e370d665901c4626eb7c2dcb85f7262b153510
Mark Pekala <mpekala@college.harvard.edu>
2023-02-05 16:07:32 -0500

    first stab server

--- server.py
+++ server.py
@@ -1,4 +1,14 @@
+import socket
+import threading
+
+HOST = "localhost"
+PORT = 65432
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind((socket.gethostname(), 80))
+serversocket.bind((HOST, PORT))
 serversocket.listen(5)
+
+while True:
+  # accept connections from outside
+  (clientsocket, address) = serversocket.accept()
+  print(clientsocket, address)
